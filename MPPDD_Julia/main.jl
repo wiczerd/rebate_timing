5a1e871ac10e2756ab384d9f9ad4d82c329ee260
David Wiczer <wiczerd@users.noreply.github.com>
2020-09-29 13:31:36 -0400

    graphing and solving in loops

--- MPPDD_Julia/main.jl
+++ MPPDD_Julia/main.jl
@@ -40,6 +40,8 @@ end
 T 	     = convertt(T, freq);
 T_retire     = convertt(T_retire, freq);
 
+qtol         = 1e-5; # tolerance for convergence in Q
+
 rebate       = 0.0;  # added to BC at t_reb (argument in backwardSolve)
 cbar         = 0.0;  # conusmption floor
 
@@ -58,6 +60,8 @@ N_s          = 6;       # Number of delinquency grid points, 5
 σ_η       = (0.01/freq)^(1/2);  # Variance of permanent income shocks = 0.01
 σ_z0      = (0.15/freq)^(1/2);  # Variance of permanent income shocks = 0.15
 σ_ε       = (0.05/freq)^(1/2);  # Variance of transitory income shocks = 0.05
+
+
 
 survivalprobs = ones(T)         # demographics
 
@@ -118,7 +122,6 @@ t_rebate = convertt(t_reb, freq); # index
 
 # two possible guess for Q
 Q0 = ones((T,N_z,N_k)).*(1/(1+r));
-Q1 = ones((T,N_z,N_k)).*(1/(1+r));
 for t=1:T
     #a0 = sum( asset_grid[t,:].<0.0 )+1
     for i = 1:N_z
@@ -128,29 +131,69 @@ for t=1:T
     end
 end
 
+# some comparative statics, solving it several times over phi parameters:
+ϕ_2grid = [1.1 1.5 2.0 3.0 5.0]
+for phi2idx = 1:5
+    ϕ_2 = ϕ_2grid[phi2idx];
+    #use last Q as a guess or go back to our initial one?
 
-for qiter = 1:40
-    V, B, C, S, Ap = backwardSolve(rebate, t_rebate, β, γ, ϕ_1, ϕ_2, r,
-        εprobs, zprobs, Ygrid, s_grid, asset_grid, T, N_k, N_s, N_z, N_ε, cbar,Q0);
+    for qiter = 1:40
+        V, B, C, S, Ap = backwardSolve(rebate, t_rebate, β, γ, ϕ_1, ϕ_2, r,
+            εprobs, zprobs, Ygrid, s_grid, asset_grid, T, N_k, N_s, N_z, N_ε, cbar,Q0);
 
-    Q = equilibriumQ(Q0, S, Ap, asset_grid, r, εprobs, zprobs);
-    Qdist = maximum( abs.(Q.-Q0) );
-    println("sup norm of distance is $Qdist")
+        Q = equilibriumQ(Q0, S, Ap, asset_grid, r, εprobs, zprobs);
+        Qdist = maximum( abs.(Q.-Q0) );
+        println("sup norm of distance is $Qdist")
 
-    #this seems to be an allocation/creation operation rather than element-wise assignment
-    # Q0 = deepcopy(Q);
-    for t=1:T
-        for i = 1:N_z
-            for api = 1:N_k
-                Q0[t,i,api] = Q[t,i,api];
+        #this seems to be an allocation/creation operation rather than element-wise assignment
+        # Q0 = deepcopy(Q);
+        for t=1:T
+            for i = 1:N_z
+                for api = 1:N_k
+                    Q0[t,i,api] = Q[t,i,api];
+                end
             end
+        end
+        if Qdist < qtol
+            break;
+        end
+    end
+    @save string("MPPDD_eqmQ_",ϕ_2,".jld2") Q0
+    @save string("MPPDD_solMats_",ϕ_2,".jld2") V, B, C, S, Ap
+
+end
+
+#+++++++++++++++++++++++++++++++++
+## quick plots:
+#+++++++++++++++++++++++++++++++++
+using Plots
+
+ϕ_2grid = [1.1 1.5 2.0 3.0 5.0]
+for phi2idx = 1:5
+    @load string("MPPDD_solMats_",ϕ_2,".jld2") V, B, C, S, Ap
+    @save string("MPPDD_eqmQ_",ϕ_2,".jld2") Q0
+    for zi=1:N_z
+
+        Qages = hcat( Q0[1,zi,:],Q0[15,zi,:],Q0[25,zi,:], Q0[35,zi,:] )
+        plot(hcat(asset_grid[2,:],asset_grid[16,:],asset_grid[26,:],asset_grid[36,:]),Qages,title = "Debt price schedule", ylabel="Equilibrium debt price", xlabel = "a'",
+            label=["Age 1" "Age 15" "Age 25" "Age 35"],legend=:bottomright, lw=3, size=(1500,600))
+        savefig(string("Q_ages_phi",ϕ_2,"_z",Zgrid[zi],".png"))
+
+        for ei=1:N_ε
+            Sages = hcat( S[2,:,ei,zi],S[15,:,ei,zi],S[25,:,ei,zi], S[35,:,ei,zi] )
+            plot(hcat(asset_grid[2,:],asset_grid[15,:],asset_grid[25,:],asset_grid[35,:]),Sages,title = "Delinquency policy", ylabel="Optimal d", xlabel = "a",
+                label=["Age 2" "Age 15" "Age 25" "Age 35"],legend=:bottomright, lw=3, size=(1500,600))
+            savefig(string("S_ages_phi",ϕ_2,"_z",Zgrid[zi],"_e",εgrid[ei],".png"))
+
+            Apages = hcat( Ap[2,:,ei,zi]-asset_grid[2,:],Ap[15,:,ei,zi]-asset_grid[15,:],Ap[25,:,ei,zi]-asset_grid[25,:], Ap[35,:,ei,zi]-asset_grid[35,:] )
+            plot(hcat(asset_grid[2,:],asset_grid[15,:],asset_grid[25,:],asset_grid[35,:]),Apages,title = "A' policy", ylabel="a'-a", xlabel = "a",
+                label=["Age 1" "Age 15" "Age 25" "Age 35"],legend=:bottomright, lw=3, size=(1500,600))
+            savefig(string("Ap_ages_phi",ϕ_2,"_z",Zgrid[zi],"_e",εgrid[ei],".png"))
         end
     end
 end
-@save "MPPDD_eqmq_newphi.jld2" Q0
 
-#quick plot for check:
-using Plots
+
 Q1 = hcat( Q0[1,1,:],Q0[1,3,:],Q0[1,6,:], )
 default(titlefont = (14, "times"), legendfont = (10,"times"), guidefont = (12, :darkgreen,"times"), tickfont = (12, :orange), framestyle = :zerolines, yminorgrid = true)
 plot(asset_grid[2,:],Q1,title = "Debt price schedule", ylabel="Equilibrium debt price", xlabel = "a'",
@@ -163,24 +206,11 @@ plot(asset_grid[2,:],D1,title = "Debt price schedule", ylabel="D", xlabel = "a",
     label=["Low z" "Medium z" "High z"],legend=:bottomright, lw=3)
 
 
-Q10 = hcat( Q0[10,1,:],Q0[10,3,:],Q0[10,6,:], )
+Q10 = hcat( Q0[10,1,:],Q0[10,3,:],Q0
+[10,6,:], )
 plot(asset_grid[10,:],Q10,title = "Debt price schedule", ylabel="Equilibrium debt price", xlabel = "a'",
     label=["Low z" "Medium z" "High z"],legend=:bottomright, lw=3)
 
 Q20 = hcat( Q0[20,1,:],Q0[20,3,:],Q0[20,6,:], )
 plot(asset_grid[20,:],Q20,title = "Debt price schedule", ylabel="Equilibrium debt price", xlabel = "a'",
     label=["Low z" "Medium z" "High z"],legend=:bottomright, lw=3)
-
-Qages = hcat( Q0[1,3,:],Q0[15,3,:],Q0[25,3,:], Q0[35,3,:] )
-plot(hcat(asset_grid[2,:],asset_grid[16,:],asset_grid[26,:],asset_grid[36,:]),Qages,title = "Debt price schedule", ylabel="Equilibrium debt price", xlabel = "a'",
-    label=["Age 1" "Age 15" "Age 25" "Age 35"],legend=:bottomright, lw=3, size=(1500,600))
-savefig("Q_ages.png")
-
-Sages = hcat( S[2,:,3,3],S[15,:,3,3],S[25,:,3,3], S[35,:,3,3] )
-plot(hcat(asset_grid[2,:],asset_grid[15,:],asset_grid[25,:],asset_grid[35,:]),Sages,title = "Delinquency policy", ylabel="Optimal d", xlabel = "a",
-    label=["Age 1" "Age 15" "Age 25" "Age 35"],legend=:bottomright, lw=3, size=(1500,600))
-savefig("S_ages_newphi.png")
-
-Apages = hcat( Ap[2,:,3,3]-asset_grid[2,:],Ap[15,:,3,3]-asset_grid[15,:],Ap[25,:,3,3]-asset_grid[25,:], Ap[35,:,3,3]-asset_grid[35,:] )
-plot(hcat(asset_grid[2,:],asset_grid[15,:],asset_grid[25,:],asset_grid[35,:]),Apages,title = "A' policy", ylabel="a'-a", xlabel = "a",
-    label=["Age 1" "Age 15" "Age 25" "Age 35"],legend=:bottomright, lw=3, size=(1500,600))
